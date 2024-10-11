a01c5b75e6830ffe3ddad71555dbbd193b2394b0
Kunal Kumar <kumarkunal8759@gmail.com>
2024-10-11 14:37:53 +0530

    improvised index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.use((req, res, next) => {
   });
   
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
